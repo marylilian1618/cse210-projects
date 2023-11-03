bb286fb3313df25c64b64882b14fc90ba30614ea
marylilian1618 <hiimlyyofgmail.com>
2023-11-03 17:45:15 -0500

    prep

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is you grade percentage? ");
+        string userdata = Console.ReadLine();
+        int grades = int.Parse(userdata);
+        string letter = "";
+        if (grades >= 90)
+        {
+            letter = "A";
+        }
+        else if (grades >= 80){
+            letter = "B";
+        }
+        else if (grades >= 70){
+            letter = "C";
+
+        }
+        else if (grades >= 60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        // sign + or -
+        int plus = grades%10;
+        string sign = "";
+
+        if (grades <= 93 && grades >= 70){
+            if (plus >= 7){
+            sign = "+";
+            }
+            else if (plus <3) {
+            sign = "-";
+            }
+            else{
+                sign = "";
+            }
+        }
+
+
+        Console.WriteLine($"Your grade is {sign}{letter}.");
+
+        if(grades >= 70){
+            Console.WriteLine("You passed the course.");
+        }
+        else{
+            Console.WriteLine("Don't give up!. You'll do next period :D");
+        }
+
+
+
+        
+
     }
 }
