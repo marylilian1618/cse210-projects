aff29d833c48907c63dec10aa932d0c3084ca0c8
marylilian1618 <hiimlyyofgmail.com>
2023-11-10 23:30:24 -0500

    Abstraction

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualBasic;
+
+public class Job{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+    public void Display(){ 
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
+
+    }
+}
